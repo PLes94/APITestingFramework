92854c4154b7abb158f3d68dfe75d8a615709dc8
Piotr Leśniewski <pls@spyro-soft.com>
2023-03-20 19:34:41 +0100

    Update Gradle Cucumber dependencies, refactor TestRunner class

--- src/main/java/my/testing/packagey/testrunner/TestRunner.java
+++ src/main/java/my/testing/packagey/testrunner/TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.core.cli.Main;
 
 import java.util.stream.Stream;
 
-public class TestRunner {
+public final class TestRunner {
     public static final String CUCUMBER_REPORTS_DIR = "cucumber-reports";
     public static final String CUCUMBER_JSON_RESULTS = CUCUMBER_REPORTS_DIR + "/cucumber-report.json";
     public static final String CUCUMBER_JUNIT_RESULTS = CUCUMBER_REPORTS_DIR + "/cucumber-junit.xml";
@@ -13,17 +13,16 @@ public class TestRunner {
             "--plugin", "json:" + CUCUMBER_JSON_RESULTS,
             "--plugin", "junit:" + CUCUMBER_JUNIT_RESULTS,
             "--plugin", "pretty",
-            "--monochrome",
-            "classpath:features" // TODO: investigate why ClassLoader is not working properly
+            "--monochrome"
     };
 
     public static void main(String[] args) {
-//        if (args.length == 0) {
-//            System.out.println("You have to pass arguments how to run the jar file.");
-//            return;
-//        }
+        if (args.length == 0) {
+            System.out.println("You have to pass arguments how to run the jar file.");
+            return;
+        }
 
         String[] cucumberOptions = Stream.of(defaultOptions, args).flatMap(Stream::of).toArray(String[]::new);
-        Main.run(cucumberOptions, Thread.currentThread().getContextClassLoader());
+        Main.run(cucumberOptions);
     }
 }
