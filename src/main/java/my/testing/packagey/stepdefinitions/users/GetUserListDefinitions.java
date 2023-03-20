c05867711b10aa85b69bb2c5ede53389c21b82c1
Piotr Leśniewski <pls@spyro-soft.com>
2023-03-20 11:13:03 +0100

    Delete the mock test prepared at the start of work

--- src/main/java/my/testing/packagey/stepdefinitions/users/GetUserListDefinitions.java
+++ src/main/java/my/testing/packagey/stepdefinitions/users/GetUserListDefinitions.java
@@ -2,25 +2,13 @@ package my.testing.packagey.stepdefinitions.users;
 
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.When;
-import io.restassured.RestAssured;
-import io.restassured.config.RestAssuredConfig;
 import my.testing.packagey.apimethods.ReqresUsersMethods;
 import my.testing.packagey.stepdefinitions.StepDefinitions;
 import my.testing.packagey.testdata.UsersTestData;
-import org.hamcrest.MatcherAssert;
-import org.hamcrest.Matchers;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GetUserListDefinitions extends StepDefinitions {
-
-    @Test
-    @DisplayName("My first mock test")
-    public void myFirstMockTest() {
-        MatcherAssert.assertThat(RestAssured.config(), Matchers.instanceOf(RestAssuredConfig.class));
-    }
 
     @When("I send request to get list of users on a specified page number")
     public void iSendRequestToGetListOfUsersOnASpecifiedPageNumber() {
